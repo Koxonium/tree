c66b3be62dff77720061e057a88fdde47cf37d25
= <toth.levente.21i@egressy.info>
2024-10-01 12:35:16 +0200

    Add project files.

---
+++ tree/Node.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tree
+{
+    public class Node
+    {
+        public int value;
+        public Node LeftChild { get; private set; }
+        public Node RightChild { get; private set; }
+        public Node(int number)
+        {
+            value = number;
+        }
+
+        /*public List<int> returnList(Node one)
+        {
+            List<int> data = new List<int>();
+            if (one.LeftChild == null)
+            {
+                data.Add(value);
+            }
+            return data;
+        }*/
+
+        public void addChild(int num)
+        {
+            if (value < num)
+            {
+                if (RightChild == null)
+                {
+                    RightChild = new Node(num);
+                }
+                else
+                {
+                    RightChild.addChild(num);
+                }
+            }
+            else
+            {
+                if (LeftChild == null)
+                {
+                    LeftChild = new Node(num);
+                }
+                else
+                {
+                    LeftChild.addChild(num);
+                }
+            }
+        }
+    }
+}
---
+++ tree/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tree
+{
+    class Program
+    {
+        static List<int> numbers = new List<int>();
+        static void Main(string[] args)
+        {
+            Node root = new Node(5);
+            root.addChild(4);
+            root.addChild(7);
+            root.addChild(3);
+            root.addChild(2);
+            root.addChild(1);
+            inorder(root);
+            foreach (int item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static void inorder(Node N)
+        {
+            if (N.LeftChild != null)
+            {
+                inorder(N.LeftChild);
+            }
+            numbers.Add(N.value);
+            if(N.RightChild != null)
+            {
+                inorder(N.RightChild);
+            }
+            return;
+        }
+    }
+}
